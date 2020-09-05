bdba4c461c663c5aefccb5e90c5643e6d316df78
wbpxre150 <wbpxre150@outlook.com>
2020-09-05 11:17:42 +0800

    major refactor

--- matrix_calcualtor.c
+++ matrix_calcualtor.c
@@ -10,10 +10,10 @@
 
 /*
  * TODO LIST
+ * add address calcualtion and print. (use sentance structure, and calc sentance) 
+ * Read input from and output to a text file in csv format, to load results into spreadsheet to view and use. 
  * add matrix of compatibility charts for iterate_months
  * add pythagoras matrix, to add name and birth numbers together to find missing numbers
- * fix bug in calc years (if it is a bug) 
- * 
 */
 
 // include needed libs. 
@@ -47,6 +47,34 @@ const int   Alt_System_Vowels[5]  = { 1, 5, 9, 6, 3 };
 const int   blk_System[21]  = { 2, 1, 5, 1, 1, 2, 3, 4, 5, 7, 8, 1, 2, 3, 4, 6, 6, 6, 6, 1, 7 };
 const int   blk_System_Vowels[5]  = { 1, 5, 1, 4, 9 };
 const int   num_days_in_month[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+// Data Structure for a birthday. 
+struct sbday {
+    char *bday;
+    int day_of_birth;
+    int reduced_day_of_birth;
+    int final_day_of_birth;
+    int month_of_birth;
+    int reduced_month_of_birth;
+    int year_of_birth;
+    int reduced_year_of_birth;
+    int final_year_of_birth;
+    int date_of_birth_number;
+    int reduced_date_of_birth_number;
+    int first_impotant_year;
+    int second_important_year;
+};
+
+// Data structure for name
+struct ssentance {
+    char *sentance;
+    int total_whole_number;
+    int reduced_total;
+    int total_vowel_number;
+    int reduced_vowel;
+    int total_consonant_number;
+    int reduced_consonant;
+};
 
 // place holder, because fuck using C++ when we dont need it.
 int dummy_consonant, dummy_vowel;
@@ -104,7 +132,7 @@ int calc_word(char* input_string, int *vowel_total, int *consonant_total) {
         if ( bvowel == 1 ) {
             *vowel_total += j;
         }
-        else if ( bvowel == 0)  { 
+        else if ( bvowel == 0 )  { 
             *consonant_total += j;
         }
         //printf("letter.%c = %d type.%d\n", input_string[len], j, bvowel);
@@ -123,7 +151,16 @@ int reduce_number(int number, int special_number) {
     return(calc_word(snumber, &dummy_vowel, &dummy_consonant));
 }
 
-void find_compatible_matches (int median_year, int range, int seeking_number) {
+int reduce_to_single_number(int number, int special_number) {
+    int ret = number;
+    while (ret > 9) {
+        ret = reduce_number(ret, special_number);
+        if ( special_number != 0 && (ret == 11 || ret == 22) ) break; // prevent infinate loop if function called incorrectly. 
+    }
+    return(ret);
+}
+
+void find_compatible_matches(int median_year, int range, int seeking_number) {
     // function to return birthdays reducing to an input number within a range of years. 
     // starting year is first number - second number. ending year is first number + second number. 
     // last number is the one being searched for. use the destiny number and the chart to find applicable numbers.
@@ -133,8 +170,7 @@ void find_compatible_matches (int median_year, int range, int seeking_number) {
             for (int z=1; z<( i % 4 == 0 && y == 1 ? num_days_in_month[y]+1 : num_days_in_month[y]); z++) {
                 char str[16];
                 sprintf(str, "%d%d%d", z, y+1, i);
-                int total_reduced_year = calc_word(str, &dummy_vowel, &dummy_consonant);
-                int reduced_year = reduce_number(total_reduced_year, IGNORE_SPECIAL_NUMBER);
+                int reduced_year = reduce_to_single_number(calc_word(str, &dummy_vowel, &dummy_consonant), IGNORE_SPECIAL_NUMBER);
                 if ( reduced_year == seeking_number ) {
                     printf("    %2d/%2d/%4d\n", z, y+1, i);
                 }
@@ -143,74 +179,75 @@ void find_compatible_matches (int median_year, int range, int seeking_number) {
     }
 }
 
-int calc_sentance (char *sentance, int *total_vowel_number, int *total_consonant_number, int *reduced_total, int *reduced_vowel, int *reduced_consonant) {
+void calc_sentance(struct ssentance *sentance) {
     // inputs sentance string, splits into words
-    // returns the total whole number of the sentance (unreduced), and everything else to the pointers in the input. 
+    // calculates and fills in variables in structure. 
+    // prints each word as it goes, this needs to output to some file format to be readable. 
     const char delim[] = " ";
-    int total_whole_number = 0;
     // assign the first word of sentance to the pointer
-    char *ptr = strtok(sentance, delim);
+    char *ptr = strtok(sentance->sentance, delim);
     if ( ptr != NULL ) {
         do {
             char word[256]; // limits word to 256 char length. 128 may be enough?
             int whole_number = 0;
             int vowel_number = 0;
             int consonant_number = 0;
-            
             // copy word into temp memory position, and calculate its totals. 
             strcpy(word, ptr);
             whole_number = calc_word(word, &vowel_number, &consonant_number);
-            
             // add this word to the total of all words. 
-            total_whole_number += whole_number;
-            *total_vowel_number += vowel_number;
-            *total_consonant_number += consonant_number;
-            
+            sentance->total_whole_number += whole_number;
+            sentance->total_vowel_number += vowel_number;
+            sentance->total_consonant_number += consonant_number;
             // reduce each word total and print the result for this word. 
             int ireduced_vowel = reduce_number(vowel_number, SPECIAL_NUMBER);
             int ireduced_consonant = reduce_number(consonant_number, SPECIAL_NUMBER);
             int ireduced_total = reduce_number(whole_number, SPECIAL_NUMBER);
             printf("%s = vowel.%d/%d consonant.%d/%d total.%d/%d\n", word, vowel_number, ireduced_vowel, consonant_number, ireduced_consonant, whole_number, ireduced_total);
-            
             // move onto the next word. 
             ptr = strtok(NULL, delim);
         } while ( ptr != NULL) ; // stop when there are no words left.
     }
     // reduce the total numbers to single digits and return the results. 
-    *reduced_total = reduce_number(total_whole_number,SPECIAL_NUMBER);
-    *reduced_vowel = reduce_number(*total_vowel_number,SPECIAL_NUMBER);
-    *reduced_consonant = reduce_number(*total_consonant_number,SPECIAL_NUMBER);
-    return(total_whole_number);
+    sentance->reduced_total = reduce_number(sentance->total_whole_number,SPECIAL_NUMBER);
+    sentance->reduced_vowel = reduce_number(sentance->total_vowel_number,SPECIAL_NUMBER);
+    sentance->reduced_consonant = reduce_number(sentance->total_consonant_number,SPECIAL_NUMBER);
 }
 
-int calc_date_of_birth_number (char *dob, int *day_of_birth_number, int *month_number, int *year_number, int *unreduced_year_number) {
-    // input string in format dd/mm/yyyy
-    // returns date of birth number, and day of birth number along with month number and reduced year number
+void calc_date_of_birth_number(struct sbday *bday) {
+    // set bday.bday = dd/mm/yyyy then call this function
+    // takes structure bday, fills all variables from input string.
     const char delim[] = "/";
     char *ptr = NULL;
-    if ( (ptr = strtok(dob, delim)) != NULL) {
+    if ( (ptr = strtok(bday->bday, delim)) != NULL) {
         char input[8];
         strcpy(input, ptr);
-        int iday_of_birth_number = calc_word(input, &dummy_vowel, &dummy_consonant);
-        *day_of_birth_number = reduce_number(iday_of_birth_number,SPECIAL_NUMBER);
+        // calculate the day of birth number
+        bday->day_of_birth = atoi(input);
+        bday->reduced_day_of_birth = reduce_number(bday->day_of_birth, IGNORE_SPECIAL_NUMBER);
+        bday->final_day_of_birth = reduce_number(bday->reduced_day_of_birth, IGNORE_SPECIAL_NUMBER);
         // calculate month number 
         if ( (ptr = strtok(NULL, delim)) != NULL) {
             strcpy(input, ptr);
-            int imonth_number = calc_word(input, &dummy_vowel, &dummy_consonant);
-            *month_number = reduce_number(imonth_number,SPECIAL_NUMBER);
+            bday->month_of_birth = atoi(input);
+            bday->reduced_month_of_birth = reduce_number(bday->month_of_birth, IGNORE_SPECIAL_NUMBER);
             // calculate the year number 
             if ( (ptr = strtok(NULL, delim)) != NULL) {
                 strcpy(input, ptr);
-                *unreduced_year_number = atoi(input);
-                *year_number = reduce_number(*unreduced_year_number,SPECIAL_NUMBER);
+                bday->year_of_birth = atoi(input);
+                bday->reduced_year_of_birth = reduce_number(bday->year_of_birth, IGNORE_SPECIAL_NUMBER);
+                bday->final_year_of_birth = reduce_to_single_number(bday->reduced_year_of_birth, IGNORE_SPECIAL_NUMBER);
+                // calculate first and possible second important years of this persons life. 
+                bday->first_impotant_year = bday->year_of_birth + bday->final_year_of_birth;
+                bday->second_important_year = bday->year_of_birth + bday->reduced_year_of_birth;
+                 // calculate the total date of birth number
+                char fulldob[16];
+                sprintf(fulldob,"%d%d%d", bday->day_of_birth, bday->month_of_birth, bday->year_of_birth);
+                bday->date_of_birth_number= calc_word(fulldob, &dummy_vowel, &dummy_consonant);
+                bday->reduced_date_of_birth_number = reduce_number(bday->date_of_birth_number, SPECIAL_NUMBER);
             } else printf("null ptr\n");
-            // calculate the total date of birth number
-            int date_of_birth_number = *day_of_birth_number + *month_number + *year_number;
-            int reduced_date_of_birth_number = reduce_number(date_of_birth_number,SPECIAL_NUMBER);
-            return(reduced_date_of_birth_number);
         } else printf("null ptr\n");
     } else printf("null ptr\n");
-    return(0);
 }
 
 void calc_personal_years (int day_of_birth_number, int month_of_birth, int year_of_birth, int dob_number, int vowel_number, int name_number, int ending_year) {
@@ -218,7 +255,11 @@ void calc_personal_years (int day_of_birth_number, int month_of_birth, int year_
     * personal years run from birthday to birthday, day of birth + month of birth + year of last birthday
     * split into 3 4 month periods, age+ year, dob_num + year, vowel_number + year. 
     * calc days, middle number (dob + name) + reduced date of the day. 
+    * 
+    * TODO: maybe this can actually start the personal year exactly on the birthday, rather than an approximate start at the next month. 
+    * example, if birthday is 1st december, the current algorithim will start personal year at 1st january, leaving 31 days in december with the wrong personal year number. 
     */
+    month_of_birth = month_of_birth == 12 ? 0 : month_of_birth; // if december start at january. 
     int middle_number = dob_number + name_number;
     int k = year_of_birth;
     for (int i=year_of_birth; i<ending_year; i++) {
@@ -227,35 +268,32 @@ void calc_personal_years (int day_of_birth_number, int month_of_birth, int year_
         char snumber[16];
         sprintf(snumber, "%d%d%d", day_of_birth_number, month_of_birth, i);
         int personal_year_number = calc_word(snumber, &dummy_vowel, &dummy_consonant);
-        int reduced_personal_year_number = reduce_number(personal_year_number,IGNORE_SPECIAL_NUMBER);
-        reduced_personal_year_number = reduced_personal_year_number>9 ? reduce_number(reduced_personal_year_number,IGNORE_SPECIAL_NUMBER) : reduced_personal_year_number;
+        int reduced_personal_year_number = reduce_to_single_number(personal_year_number,IGNORE_SPECIAL_NUMBER);
         printf("New Year.%d = %d/%d\n", i, personal_year_number, reduced_personal_year_number);
-        for (int y=(month_of_birth == 11 ? 0 : month_of_birth); y<12; y++) {
+        for (int y=month_of_birth; y<12; y++) {
             // increase k by one each january, to display the actual date in the prints, to allow searching for a particular day. 
             if ( y == 0 ) k++;
             // calculations for each 4 month period. 
             if ( j == 0 ) {
                 // calculate age, add age to year. 
                 int age = i-year_of_birth;
-                printf("    First 4 month period = %d\n", reduce_number(reduce_number(age+i, IGNORE_SPECIAL_NUMBER), IGNORE_SPECIAL_NUMBER));
+                printf("    First 4 month period = %d\n", reduce_to_single_number(age+i, IGNORE_SPECIAL_NUMBER));
             } else if ( j == 4) {
                 // add date of birth number to year
-                printf("    Second 4 month period = %d\n", reduce_number(reduce_number(dob_number+i, IGNORE_SPECIAL_NUMBER),IGNORE_SPECIAL_NUMBER));
+                printf("    Second 4 month period = %d\n", reduce_to_single_number(dob_number+i, IGNORE_SPECIAL_NUMBER));
             } else if ( j == 8) {
                 // add vowel number to year
-                printf("    Third 4 month period = %d\n", reduce_number(reduce_number(vowel_number+i, IGNORE_SPECIAL_NUMBER),IGNORE_SPECIAL_NUMBER));
+                printf("    Third 4 month period = %d\n", reduce_to_single_number(vowel_number+i, IGNORE_SPECIAL_NUMBER));
             }
             // calculations for each month 
             int personal_month_number = personal_year_number+y+1;
-            int reduced_personal_month_number = reduce_number(personal_month_number,IGNORE_SPECIAL_NUMBER);
-            reduced_personal_month_number = reduced_personal_month_number>9 ? reduce_number(reduced_personal_month_number,IGNORE_SPECIAL_NUMBER) : reduced_personal_month_number;
+            int reduced_personal_month_number = reduce_to_single_number(personal_month_number,IGNORE_SPECIAL_NUMBER);
             printf("        New Month.%d = %d/%d\n", y+1, personal_month_number, reduced_personal_month_number);
             for (int z=0; z<( i % 4 == 0 && y == 1 ? num_days_in_month[y]+1 : num_days_in_month[y]); z++) {
                 // calculations for each day
                 sprintf(snumber, "%d%d%d", z+1, y+1, i);
                 int day_number = calc_word(snumber, &dummy_vowel, &dummy_consonant);
-                int actual_day_number = reduce_number(day_number+middle_number,IGNORE_SPECIAL_NUMBER);
-                actual_day_number = actual_day_number>9 ? reduce_number(actual_day_number,IGNORE_SPECIAL_NUMBER) : actual_day_number;
+                int actual_day_number = reduce_to_single_number(day_number+middle_number,IGNORE_SPECIAL_NUMBER);
                 printf("            %d/%d/%d = %d + %d = %d/%d\n", z+1, y+1 ,k, day_number, middle_number, day_number+middle_number, actual_day_number);
             }
             y = y == 11 ? -1 : y;
@@ -266,41 +304,34 @@ void calc_personal_years (int day_of_birth_number, int month_of_birth, int year_
 
 int main() {
     // inputs to calculate
-    char name[] = "Adam Bullock";
-    char bday[] = "28/6/1988"; 
+    char strname[] = "Adam Bullock";
+    char strbday[] = "28/6/1988";
     int  max_age_gap = 4;
     
-    // needed variables
-    int total_whole_number = 0;
-    int total_vowel_number = 0;
-    int total_consonant_number = 0;
-    int reduced_total = 0;
-    int reduced_vowel = 0;
-    int reduced_consonant = 0;
-    int unreduced_year_number = 0;
-    int year_number = 0;
-    int month_number = 0;
-    int day_of_birth_number = 0;
-    int date_of_birth_number = 0;
+    // structures for name and birthday, set all variables to 0.
+    struct sbday bday;
+    memset(&bday, 0, sizeof(bday));
+    struct ssentance name;
+    memset(&name, 0, sizeof(name));
+    // assign birthday string and name string to structures. 
+    name.sentance = strname;
+    bday.bday = strbday;
     
-    // calculate name using calc_sentance function. 
-    total_whole_number = calc_sentance(name, &total_vowel_number, &total_consonant_number, &reduced_total, &reduced_vowel, &reduced_consonant);
-    printf("vowel number.%d/%d\nconesnant number.%d/%d\nwhole name number.%d/%d\n\n", total_vowel_number, reduced_vowel, total_consonant_number, reduced_consonant, total_whole_number, reduced_total);
+    // calculate and print name 
+    calc_sentance(&name);
+    printf("vowel number.%d/%d\nconesnant number.%d/%d\nwhole name number.%d/%d\n\n", name.total_vowel_number, name.reduced_vowel, name.total_consonant_number, name.reduced_consonant, name.total_whole_number, name.reduced_total);
     
-    // calculate day of birth number
-    date_of_birth_number = calc_date_of_birth_number(bday, &day_of_birth_number, &month_number, &year_number, &unreduced_year_number);
-    printf("day.%d month.%d year.%d/%d\n  date_of_birth_number.%d\n", day_of_birth_number, month_number, unreduced_year_number, year_number,date_of_birth_number);
-    
-    // calculate and print the important years of this persons life.
-    printf("first important year.%d\nsecond important year.%d\n\n", reduce_number(year_number,SPECIAL_NUMBER)+unreduced_year_number, unreduced_year_number+year_number);
+    // calculate and print the date of birth
+    calc_date_of_birth_number(&bday);
+    printf("day.%d/%d/%d month.%d/%d year.%d/%d/%d\n  date_of_birth_number.%d/%d\n  first important year.%d\n  second important year.%d\n\n", bday.day_of_birth, bday.reduced_day_of_birth,bday.final_day_of_birth,  bday.month_of_birth, bday.reduced_month_of_birth, bday.year_of_birth, bday.reduced_year_of_birth, bday.final_year_of_birth, bday.date_of_birth_number, bday.reduced_date_of_birth_number, bday.first_impotant_year, bday.second_important_year);
     
     // find and print personal year, 4 month periods, months and days. 
-    printf("Personal Year, thirds of year, month and day calcualtions:\n");
-    // possible bug here, as month number needs to be unreduced for the months, 10, 11, 12. Needs further testing and a fix. 
-    calc_personal_years (day_of_birth_number, month_number, unreduced_year_number, date_of_birth_number, reduced_vowel, reduced_total, 2088);
+    printf("Personal Year, thirds of year, month and day calcultions:\n");
+    calc_personal_years (bday.final_day_of_birth, bday.month_of_birth, bday.year_of_birth, bday.reduced_date_of_birth_number, name.reduced_vowel, name.reduced_total, 2022);
     
     // find compatible business or personal matches based on date of birth only. You need a chart to determine what number to seek for until I add it to be done automatically. 
     printf("\nCompatible dates of birth for business or personal relationships:\n");
-    find_compatible_matches(unreduced_year_number, max_age_gap, 9);
+    find_compatible_matches(bday.year_of_birth, max_age_gap, 9);
+    
     return(0);
 }
